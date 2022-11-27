ca6e7f513eb4c3f30aa1d8f2e66988d064fad3ab
wizi-tag <a.kaplyash@gmail.com>
2022-11-26 19:15:52 -0600

    qrcode download

--- src/qrcode/qrcode.controller.ts
+++ src/qrcode/qrcode.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { QRCodeService } from './qrcode.service';
+import { Response, response } from 'express';
 
 @Controller('qrcode')
 export class QRCodeController {
   constructor(public readonly qrcodeService: QRCodeService) {}
 
   @Get('generate')
-  async find(@Query('url') url: string) {
-    return await this.qrcodeService.generate(url)
+  async find(@Query('id') id: string, @Res() res: Response ) {
+    const filename = await this.qrcodeService.generate(id);
+    console.log(filename);
+    setTimeout(function() {
+      res.download(filename);
+    }, 500);
+    console.log(filename);
   }
 
 }
--- src/qrcode/qrcode.service.ts
+++ src/qrcode/qrcode.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import  * as QRCode from 'qrcode';
+import * as path from 'path';
+require('dotenv').config();
 
 @Injectable()
 export class QRCodeService{
     constructor(){}
 
-    async generate(url:string) {
-        QRCode.toFile('./qr.png', url, function (err) {
+    async generate(id:string) {
+        const host = process.env.FRONTEND_HOST;
+        const url = host + "/" + id;
+        console.log(url);
+        const filename = path.resolve('./qrcodes/'+id+'.png');
+        await QRCode.toFile(filename, url, function (err) {
             if (err) throw err
             console.log('QR Code created')
           })
+        return filename;
     }
 }
 
